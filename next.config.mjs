97d32f32fd0c0e32413892d4efe0ec8fbfacae49
Faishal Irawan <muhammadfaishal193@gmail.com>
2024-11-10 10:36:14 +0700

    add mock data

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,7 @@ const nextConfig = {
         remotePatterns: [
             {
                 protocol: "https",
-                hostname: "static.promediateknologi.id",
+                hostname: "food-cms.grab.com",
             },
         ],
     },
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,34 +1,25 @@
 import Image from "next/image";
+import data from "@/data/mock_data.json";
 
 export default function Home() {
     return (
         <main>
             <h1 className="font-bold">Menu</h1>
             <section className="grid grid-cols-2 gap-3">
-                <div className="border">
-                    <Image
-                        src="https://static.promediateknologi.id/crop/0x0:0x0/750x500/webp/photo/p1/79/2023/10/30/WhatsApp-Image-2023-10-30-at-110613-3771525713.jpeg"
-                        alt="pizza"
-                        width={60}
-                        height={60}
-                    />
-                    <div>
-                        <p>Pizza</p>
-                        <p>Rp190.000</p>
+                {data.map((food) => (
+                    <div key={food.id} className="border">
+                        <Image
+                            src={food.picture}
+                            alt={food.name}
+                            width={60}
+                            height={60}
+                        />
+                        <div>
+                            <p>{food.name}</p>
+                            <p>Rp{food.price}</p>
+                        </div>
                     </div>
-                </div>
-                <div className="border">
-                    <Image
-                        src="https://static.promediateknologi.id/crop/0x0:0x0/750x500/webp/photo/p1/79/2023/10/30/WhatsApp-Image-2023-10-30-at-110613-3771525713.jpeg"
-                        alt="pizza"
-                        width={60}
-                        height={60}
-                    />
-                    <div>
-                        <p>Pizza</p>
-                        <p>Rp190.000</p>
-                    </div>
-                </div>
+                ))}
             </section>
         </main>
     );
