2efb14aa284d0e8b589fcf7fa2b08820c224ba45
Faishal Irawan <muhammadfaishal193@gmail.com>
2024-11-10 20:27:33 +0700

    feat: add unit test

---
+++ __test__/page.test.tsx
@@ -0,0 +1,13 @@
+import "@testing-library/jest-dom";
+import { render, screen } from "@testing-library/react";
+import Home from "@/app/page";
+
+describe("Page", () => {
+    it("renders juna's restaurant heading", () => {
+        render(<Home />);
+
+        const heading = screen.getByText("Juna Restaurant's Menu");
+
+        expect(heading).toBeInTheDocument();
+    });
+});
